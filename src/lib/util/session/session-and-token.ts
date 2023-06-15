ad379707699a5a11162852a663995746151af779
Welwei | Tilman <tilman.sattler@outlook.de>
2023-06-15 15:40:27 +0200

    feature/actions fix type errors

--- src/lib/util/session/session-and-token.ts
+++ src/lib/util/session/session-and-token.ts
@@ -2,7 +2,7 @@ import { AuthOptions, Session, getServerSession } from "next-auth";
 import { JWT, getToken } from "next-auth/jwt";
 import { NextRequest } from "next/server";
 
-export async function getUserSessionAndToken(authOptions: AuthOptions, request: NextRequest): Promise<UserSessionAndToken | null> {
+export async function getUserSessionAndToken(authOptions: AuthOptions, request: NextRequest): Promise<any | null> {
     const userSession: Session | null = await getServerSession(authOptions);
     const userToken: JWT | null = await getToken({ req: request });
 
