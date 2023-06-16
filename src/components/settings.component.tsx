4c3922881bf839c78650b881e36981c9a877a367
Pascal Dantinger <pascal@dantinger.de>
2023-06-16 22:07:41 +0200

    impl settings view

---
+++ src/components/settings.component.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { useState } from "react";
+import "@/components/css/settings.css";
+
+export default function Settings({}) {
+  return (
+    <div>
+     <h1>to do</h1>
+    </div>
+  );
+}
