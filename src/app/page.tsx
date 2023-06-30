5b381601a7b5c368653b3d0f2ba36c4d38cd37be
Pascal Dantinger <pascal@dantinger.de>
2023-06-30 10:06:45 +0200

    changed app style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,36 @@
 "use client";
 
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 import Lottie from "react-lottie-player";
-import beehive from '@/animations/beehive-loader.json';
+import beehive from "@/animations/beehive-loader.json";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
+import "@/components/css/app.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default async function Home() {
-
   const session = useSession();
 
   const router: AppRouterInstance = useRouter();
 
-  if (session.status === 'loading') {
+  if (session.status === "loading") {
     return (
       <main>
         <div className="loading-spinner-wrapper">
-          <Lottie play loop animationData={beehive} style={{ width: 400, height: 400 }}/>
+          <Lottie
+            play
+            loop
+            animationData={beehive}
+            style={{ width: 400, height: 400 }}
+          />
         </div>
       </main>
     );
-  } else if (session.status === 'unauthenticated') {
-    router.push('/login');
+  } else if (session.status === "unauthenticated") {
+    router.push("/login");
   } else {
-    router.push('/chats');
+    router.push("/chats");
   }
 }
